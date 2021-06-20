d5bd7b4db10746951615dacc6e677190121a9c2f
prodhc <zionywood@gmail.com>
2021-06-20 14:08:38 -0700

    set up

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//DEPENDENCIES
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+
+//COMPONETNS
+import Home from './components/Home'
+import Login from './components/User/Login'
+import Register from './components/User/Register'
+
+function App() {
+  return (
+    <Router>
+      <Switch>
+      <Route path='/register'  component={Register}/>
+        <Route path='/login'  component={Login}/>
+        <Route path='/' component={Home} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            POTLUCK VAULT
+        </div>
+    )
+}
+
+export default Home
