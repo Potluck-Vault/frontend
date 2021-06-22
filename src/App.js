d601c1c18f9e71927e6c7d2deb36c17139f04d98
Robert Taveras <monkeyeden@Roberts-MBP.lan>
2021-06-22 12:29:20 -0400

    Register page

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Home from './components/Home'
 import Login from './components/User/Login'
 import Register from './components/User/Register'
 
+
 function App() {
   return (
     <Router>
       <Switch>
-      <Route path='/register'  component={Register}/>
+        <Route path='/register'  component={Register}/>
         <Route path='/login'  component={Login}/>
         <Route path='/' component={Home} />
       </Switch>
---
+++ src/components/User/Item.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Item = (props) =>{
+    const {item} = props
+    console.log(props)
+
+    return (
+        <div>
+            <h3>Awesome</h3>
+        </div>
+    ) 
+}
+export default Item;
