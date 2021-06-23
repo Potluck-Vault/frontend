5b62cb0b5414579129af48ae4e9121a77c52f21a
MurrayWarnock <murray.warnock@gmail.com>
2021-06-23 14:09:52 -0600

    Start of Home page; pulling data for Home and Edit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 //DEPENDENCIES
-import { BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import React, { useState } from 'react'
+import axios from 'axios';
 import {v4 as uuidv4} from 'uuid';
 
 //COMPONETNS
@@ -10,54 +11,61 @@ import Register from './components/User/Register'
 import AddPotluck from './components/Potlucks/AddPotluck'
 
 function App() {
-  const [potlucks, setPotlucks ] = useState([])
+  // const [potlucks, setPotlucks] = useState([])
+  // const [items, setItems] = useState([]);
+	// const [guests, setGuests] = useState([]);
 
-  const [items, setItems] = useState([]);
-	const [guests, setGuests] = useState([]);
-
-	const [potluck, setPotluck] = useState({
-        id: uuidv4(),
-        title:"",
-        date: "",
-        time: "",
-        location: "",
-        description: "",
-        items: items,
-        guests: guests
-	});
-// console.log("potluck right after creation in App: ", potluck);
-  // setPotlucks([...potlucks, potluck]);
+  // // const [potluck, setPotluck] = useState({});
+	// const [potluck, setPotluck] = useState({
+  //       id: uuidv4(),
+  //       title:"",
+  //       date: "",
+  //       time: "",
+  //       location: "",
+  //       description: "",
+  //       items: items,
+  //       guests: guests
+	// });
 
   return (
     <Router>
       <Switch>
         <Route path='/register'  component={Register}/>
         <Route path='/login'  component={Login}/>
-        <Route exact path='/' component={Home} />
-        <Route path='/addpotluck' 
-              render={(props) => (
-                <AddPotluck {...props} potlucks={potlucks} 
-                            setPotlucks={setPotlucks}
-                            potluck={potluck} 
-                            setPotluck={setPotluck} 
-                            items={items} 
-                            setItems={setItems} 
-                            guests={guests}
-                            setGuests={setGuests}  />
-              )}
-        />
-        <Route path='/editpotluck' 
-              render={(props) => (
-                <AddPotluck {...props} potlucks={potlucks} 
-                            setPotlucks={setPotlucks}
-                            potluck={potluck} 
-                            setPotluck={setPotluck} 
-                            items={items} 
-                            setItems={setItems} 
-                            guests={guests}
-                            setGuests={setGuests}  />
-              )}
-        />
+        <Route exact path='/'  component={Home}/>
+        {/* <Route exact path='/'
+          render={(props) => (
+            <Home {...props} potlucks={potlucks} 
+                  setPotlucks={setPotlucks} />
+          )} 
+          /> */}
+
+        <Route path='/addpotluck'  component={AddPotluck}/>
+        <Route path='/editpotluck/:id'  component={AddPotluck}/>
+        {/* <Route path='/addpotluck' 
+          render={(props) => (
+            <AddPotluck {...props} potlucks={potlucks} 
+                        setPotlucks={setPotlucks}
+                        potluck={potluck} 
+                        setPotluck={setPotluck} 
+                        items={items} 
+                        setItems={setItems} 
+                        guests={guests}
+                        setGuests={setGuests}  />
+          )}
+            /> */}
+        {/* <Route path='/editpotluck' 
+          render={(props) => (
+            <AddPotluck {...props} potlucks={potlucks} 
+                        setPotlucks={setPotlucks}
+                        potluck={potluck} 
+                        setPotluck={setPotluck} 
+                        items={items} 
+                        setItems={setItems} 
+                        guests={guests}
+                        setGuests={setGuests}  />
+          )}
+        /> */}
       </Switch>
     </Router>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,96 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Link, useHistory } from 'react-router-dom';
+import axios from 'axios';
+import {v4 as uuidv4} from 'uuid';
+
+import AddPotluck from "./Potlucks/AddPotluck"
 
 const Home = () => {
+    const { push } = useHistory();
+    const [potlucks, setPotlucks] = useState([])
+    // const [items, setItems] = useState([]);
+    // const [guests, setGuests] = useState([]);
+  
+    const [potluck, setPotluck] = useState({
+        id: uuidv4(),
+        title:"",
+        date: "",
+        time: "",
+        location: "",
+        description: "",
+        items: [],
+        guests: []
+    });
+
+    const editPotluck = (potluck) => {
+        push("/editpotluck");
+
+        console.log("editPotluck potluck obj: ", potluck);
+        return(
+
+        <AddPotluck setPotluck={setPotluck} 
+                    potluck={potluck} 
+                    potlucks={potlucks}  
+                    setPotlucks={setPotlucks} />
+    )};
+
+   useEffect(()=>{
+        axios.get('https://potluckvaultv2.herokuapp.com/api/potlucks')
+          .then(res => { 
+            setPotlucks(res.data);
+          })
+          .catch(err => {
+            console.log(err);
+          });
+          
+      }, []);
     return (
         <div>
-            POTLUCK VAULT
+            <h1>My Potlucks</h1>
+            {potlucks.map(potluck => {
+            return (
+            <div className="potluck-card">
+                <h2>{potluck.name}</h2>
+                {/* Link with onClick and component */}
+                {/* <Link to={'/editpotluck'} component={<AddPotluck />} onClick={()=>{editPotluck(potluck)}}>Edit</Link> */}
+
+                {/* Link with component; no onClick */}
+                {/* <Link to={'/editpotluck'} component={AddPotluck}>Edit</Link> */}
+
+                {/* Button with onClick */}
+                {/* <button onClick={()=>{editPotluck(potluck)}}>Edit</button> */}
+
+                {/* Link with id in url */}
+                <Link to={`/editpotluck/${potluck.id}`}>Edit</Link>
+
+                <p>Date : {potluck.date}</p>
+                <p>Time : {potluck.time}</p>
+                <p>Location : {potluck.location}</p>
+                <p>Description : {potluck.description}</p>
+                <div className="potluck-items">
+                    <h3>Menu Items</h3>
+                    {potluck.items.map(item => {
+                        return(
+                            <div>
+                                <p>{item.item}</p> 
+                                <p>Claimed?: {item.claimed}</p>
+                                <p>By: {item.claimedBy}</p>
+                            </div>
+                    )})}
+                    <h3>Guests</h3>
+                    {potluck.guests.map(guest => {
+                        return(
+                            <div className="guest">
+                                <p>{guest.guest}</p>
+                                <p>{guest.contact}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>)}
+            )}
         </div>
+
     )
 }
 
